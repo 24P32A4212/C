e453935387fa7a45fab67c21dfd48d9989dbdcbf
ABHINAV <naliqbhi@gmail.com>
2026-01-02 19:05:34 +0530

    Armstrong number

---
+++ Armstrong number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, temp, rem, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+    while (temp != 0) {
+        rem = temp % 10;
+        sum += rem * rem * rem;
+        temp /= 10;
+    }
+
+    if (sum == n)
+        printf("Armstrong number");
+    else
+        printf("Not an Armstrong number");
+
+    return 0;
+}
