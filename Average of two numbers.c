04d8123380b744394f29a0beadc5694284cff4ab
ABHINAV <naliqbhi@gmail.com>
2026-03-09 19:57:16 +0530

    Add program to calculate average of multiple numbers

---
+++ Average of two numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float num, sum = 0, avg;
+
+    printf("How many numbers: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        printf("Enter number: ");
+        scanf("%f", &num);
+        sum += num;
+    }
+
+    avg = sum / n;
+
+    printf("Average = %.2f", avg);
+
+    return 0;
+}
